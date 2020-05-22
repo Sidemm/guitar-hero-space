f81e9f8fa5cc3b2c328e9125a95958317b8af584
Sidemm <41542904+Sidemm@users.noreply.github.com>
2020-05-22 13:11:38 +0300

    Add files via upload

---
+++ MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public PlayerController playerShip;
+	public GameObject mainMenu;
+	public AudioSource gamemusic;
+	public AudioSource menumusic;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void OnStartPressed()
+	{
+		mainMenu.SetActive (false);
+		playerShip.enabled = true;
+		gamemusic.enabled = true;
+		menumusic.enabled = false;
+	}
+}
